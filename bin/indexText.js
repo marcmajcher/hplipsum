2486d1fd1ce7b921a4209f38369a7be1de89346b
Marc Majcher <majcher@gmail.com>
2015-04-27 17:40:43 -0500

    backtrack

--- bin/indexText.js
+++ bin/indexText.js
@@ -11,69 +11,65 @@ console.log('Indexing text');
 var dataDir = 'data/';
 var textDir = dataDir + 'text/';
 var indexFile = dataDir + 'index.json';
-// var index = {
-//	__seed: []
-// };
-var fileNames;
+var index = {
+	__seed: []
+};
 var fileCount;
 
-fs.readdir(textDir, function(err, files) {
-	fileNames = files;
-	fileCount = files.length;
-});
+var populateDB = function(index) {
 
-var index;
-var db;
+	var db = new Db('hplipsum', new Server('localhost', mongoPort));
+	db.open(function(err, db) {
+		if (err) {
+			console.log('ERROR: Can\'t connect to mongoDB on port ' + mongoPort);
+			throw (err);
+		}
+
+		db.collection('index', function(err, collection) {
+			if (err) {
+				console.log('ERROR: Can\'t create collection');
+				throw (err);
+			}
+			collection.remove();
+			//index = {dammit: 123};
+			// use mongoimport instead?
+			
+			console.log(index);
+			collection.insert(index);
+		});
+		db.close();
+		console.log('Created index in db');
+
+	});
+};
 
 var processFile = function(file) {
-	console.log('Processing: ' + file);
-
 	fs.readFile(textDir + file, function(err, data) {
 		var words = String(data).split(/\s+/);
-		// index.__seed.push([words[0], words[1]]);
-		// words.forEach(function(word, i) {
-		// 	if (i <= words.length - 3) {
-		// 		if (!index[word]) {
-		// 			index[word] = {};
-		// 		}
-		// 		index[word][words[i + 1]] = words[i + 2];
-		// 	}
-		// });
-	// http://mongodb.github.io/node-mongodb-native/api-generated/collection.html#remove
-
-		index.insert({title: file, length: words.length});
-
+		index.__seed.push([words[0], words[1]]);
+		words.forEach(function(word, i) {
+			if (i <= words.length - 3) {
+				if (!index[word]) {
+					index[word] = {};
+				}
+				index[word][words[i + 1]] = words[i + 2];
+			}
+		});
 		fileCount--;
 		if (fileCount === 0) {
-			//	populateDB(index);
-			//	// fs.writeFile(indexFile, JSON.stringify(index), function(err) {
-			//	//	if (err) {
-			//	//		throw(err);
-			//	//	}
-			//	//	console.log('Index file saved to '+indexFile);
-			//	// });
-			db.close();
-			console.log('Text files indexed.');
+			populateDB(index);
+			// fs.writeFile(indexFile, JSON.stringify(index), function(err) {
+			//	if (err) {
+			//		throw(err);
+			//	}
+			//	console.log('Index file saved to '+indexFile);
+			// });
 		}
 	});
 };
 
-db = new Db('hplipsum', new Server('localhost', mongoPort));
-db.open(function(err, db) {
-	if (err) {
-		console.log('ERROR: Can\'t connect to mongoDB on port ' + mongoPort);
-		throw (err);
-	}
 
-	db.collection('index', function(err, collection) {
-		if (err) {
-			console.log('ERROR: Can\'t create collection');
-			throw (err);
-		}
-
-		index = collection;
-		index.remove();
-
-		fileNames.forEach(processFile);
-	});
+fs.readdir(textDir, function(err, files) {
+	fileCount = files.length;
+	files.forEach(processFile);
 });

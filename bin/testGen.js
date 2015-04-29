3c50d25700af8201d8250915cb9f22a1ca58e052
Marc Majcher <majcher@gmail.com>
2015-04-29 15:23:02 -0500

    bones of ipsum generator

--- bin/testGen.js
+++ bin/testGen.js
@@ -3,6 +3,9 @@ var Db = require('mongodb').Db;
 var Server = require('mongodb').Server;
 var mongoPort = 27017;
 
+var words = process.argv[2] || 250;
+var paras = process.argv[3] || 1;
+
 var db = new Db('hplipsum', new Server('localhost', mongoPort));
 db.open(function(err, db) {
 	if (err) {
@@ -16,11 +19,32 @@ db.open(function(err, db) {
 			throw (err);
 		}
 
+		/* Choose a random paragraph seed */
 		index.findOne({
 			_id: '_seeds'
 		}, function(err, object) {
-			console.log(object);
-			db.close();
+			var seed = object.seeds[Math.floor(Math.random() * object.seeds.length)];
+
+			console.log('Seed: ' + seed.join(' '));
+
+			index.find({
+					w1: seed[0],
+					w2: seed[1]
+				},
+				function(err, object) {
+					object.toArray(function(err, docs) {
+						if (err) {
+							throw err;
+						}
+
+						var w3 = docs[Math.floor(Math.random() * docs.length)].w3;
+						seed.push(w3);
+						console.log(seed.join(' '));
+						db.close();
+					});
+
+				});
+
 		});
 
 

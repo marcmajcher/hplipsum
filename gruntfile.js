d2351b55a0d28fa516e86086dac74355ed103a53
Marc Majcher <majcher@gmail.com>
2015-04-30 01:31:23 -0500

    grunt build

--- gruntfile.js
+++ gruntfile.js
@@ -177,7 +177,7 @@ module.exports = function(grunt) {
 	grunt.registerTask('lint', ['jshint', 'csslint']);
 
 	// Build task(s).
-	grunt.registerTask('build', ['lint', 'index', 'loadConfig', 'ngAnnotate', 'uglify', 'cssmin']);
+	grunt.registerTask('build', ['lint', /*'index',*/ 'loadConfig', 'ngAnnotate', 'uglify', 'cssmin']);
 
 	// Test task.
 	grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);

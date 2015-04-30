ca808b312fd651508274213a1087cd9f7031831d
Marc Majcher <majcher@gmail.com>
2015-04-30 16:44:30 -0500

    more styling

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', '$http',
-	function($scope, $http) {
+angular.module('core').controller('HomeController', ['$scope', '$http', '$interval',
+	function($scope, $http, $interval) {
 
 		var loadText = ['Dancing madly around Azathoth...', 'Frenzied bodies heaving savagely...',
 						'Removing Nyarlathotep\'s masks...', 'Chanting the Unspeakable Oath...',
@@ -10,24 +10,45 @@ angular.module('core').controller('HomeController', ['$scope', '$http',
 		var maxWords = 750;
 		var maxParas = 12;
 
-		$scope.hplipsum = [];
 		$scope.wordCount = 100;
 		$scope.paraCount = 2;
+		$scope.loadingText = '';
+		$scope.hplipsum = [];
+
+		var loaderInterval;
+		var summoning = false;
 
 		var setLoadText = function() {
-			$scope.hplipsum = [ loadText[Math.floor(Math.random()*loadText.length)] ];
+			$scope.loadingText = loadText[Math.floor(Math.random() * loadText.length)];
+		};
+
+		var startLoadText = function() {
+			setLoadText();
+			loaderInterval = $interval(setLoadText, 1000);
+		};
+
+		var stopLoadText = function() {
+			$scope.loadingText = '';
+			$interval.cancel(loaderInterval);
 		};
 
 		$scope.generate = function() {
+			if (summoning) {
+				return;
+			}
+			summoning = true;
+			startLoadText();
+			$scope.hplipsum = [];
 			$scope.wordCount = Math.min(maxWords, $scope.wordCount);
 			$scope.paraCount = Math.min(maxParas, $scope.paraCount);
 			$http.get('/ipsum/' + $scope.wordCount + '/' + $scope.paraCount).success(function(data) {
 				$scope.hplipsum = data;
+				stopLoadText();
+				summoning = false;
 			});
 		};
 
-		setLoadText();
 		$scope.generate();
-		
+
 	}
 ]);

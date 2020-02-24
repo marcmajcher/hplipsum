3ced37fc871feacc54b33e8fe39034f6a2e35e55
Marc Majcher <majcher@gmail.com>
2020-02-24 17:41:56 -0600

    removing terribleness

--- src/TextGenerator.js
+++ src/TextGenerator.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 function select(arr) {
+  if (arr === undefined) {
+    console.log('ARRRRRGH UNDEF', arr);
+  }
   return arr[Math.floor(Math.random() * arr.length)];
 }
 function generateText(chain, numParas, numWords) {
@@ -17,16 +20,32 @@ function generateText(chain, numParas, numWords) {
 function generateParagraph(chain, numWords) {
   let text = select(chain._seeds);
   let [w1, w2] = text;
-  for (let i = 0; i < numWords || !text[text.length - 1].endsWith('.'); i++) {
+  for (
+    let i = 0;
+    i < numWords ||
+    text[text.length - 1][text[text.length - 1].length - 1] !== '.';
+    i++
+  ) {
     const w3 = getNextWord(chain, w1, w2);
+    if (typeof(w3) !== 'string') {
+      break;
+    }
     text.push(w3);
     [w1, w2] = [w2, w3];
+    if (typeof text[text.length - 1] !== 'string') {
+      console.error('WATTTTTTTTTTTTTT');
+    }
+    console.log("==> ",text[text.length - 1])
+    console.log("I",i, "DOT:",text[text.length - 1][text[text.length - 1].length - 1] !== '.')
   }
 
   return text.join(' ');
 }
 
 function getNextWord(chain, w1, w2) {
+  if (chain[w1] === undefined || chain[w1][w2] === undefined) {
+    return select(chain._seeds);
+  }
   return select(chain[w1][w2]);
 }
 

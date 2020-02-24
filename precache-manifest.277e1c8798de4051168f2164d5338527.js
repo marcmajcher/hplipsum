cc3443c01431db5b5ce4fe06d7b37cef83ef6154
Marc Majcher <majcher@gmail.com>
2020-02-24 13:32:10 -0600

    Updates

---
+++ precache-manifest.277e1c8798de4051168f2164d5338527.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4f0b22447919ec99b91d550a8994a0",
+    "url": "/hplipsum/index.html"
+  },
+  {
+    "revision": "e5911b42b4fef39a879e",
+    "url": "/hplipsum/static/css/main.3a4e7a8f.chunk.css"
+  },
+  {
+    "revision": "dbccb21aa0de83154755",
+    "url": "/hplipsum/static/js/2.04054d45.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hplipsum/static/js/2.04054d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5911b42b4fef39a879e",
+    "url": "/hplipsum/static/js/main.fb4064b1.chunk.js"
+  },
+  {
+    "revision": "d7f19f27572b48dbabe1",
+    "url": "/hplipsum/static/js/runtime-main.70da2158.js"
+  }
+]);
